d8347f58f83bbb13d0c63ee01b2609103571589a
lchancel <lchancel@etna-alternance.net>
2019-08-20 22:40:08 +0200

    ajout #define

--- headerFiles/fonts.h
+++ headerFiles/fonts.h
@@ -1,22 +1,11 @@
 #ifndef FONT_H
 #define FONT_H
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
-
 #define PATHFGRAFITI "assets/font/Grafiti.ttf"
 #define PATHFNEON "assets/font/Neon.ttf"
 
 #define TXTSURFHOSTNAME "Tapez l'hostname suivi d'entree :"
 #define TXTSURFPORTNAME "Tapez le numero de port puis entree : "
-#define FONTCOLOR {0,0,0}
-
-
-#define LEN(arr)  ( (int) (sizeof(arr)/sizeof(arr[0])) )
-
-typedef struct stFont {
-    SDL_Surface *surface;
-    SDL_Texture *texture;
-} stFont;
+#define TXTWELCOMESERVEUR "Bienvenue, tapez une commande pour le server"
 
 #endif
