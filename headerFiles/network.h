d43c03519c660c8739c05fd7cb9c919277b6df95
lchancel <lchancel@etna-alternance.net>
2019-08-27 14:30:58 +0200

    #define maxclient, struct stTrackClient, fonctions read_*, track_client et create_track

--- headerFiles/network.h
+++ headerFiles/network.h
@@ -11,9 +11,24 @@
 #include <sys/time.h>
 #include <string.h>
 
+#define MAXCLIENT 4
+
+typedef struct stTrackClient {
+    int max_sd;
+    int new_socket;
+    int sd;
+} stTrackClient;
+
+int my_strlen(char *str);
+int read_client(int client);
+int read_server(int server);
+void track_client(int *server_socket);
 int load_server(int *my_socket, char *hostname, char *portname);
+<<<<<<< HEAD
 int create_track_client(int *my_socket, int max_client, int clients_array[]);
 int load_client(int *mysocket, char *hostname, char *portname);
+=======
+int create_track_client(int *my_socket, int clients_array[]);
+>>>>>>> #define maxclient, struct stTrackClient, fonctions read_*, track_client et create_track
 int read_client(int client);
-
 #endif
